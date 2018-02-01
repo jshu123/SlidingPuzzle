70075b19ca0f188318c1fe15c740b9de770b6196
jshu123 <jshu@pdx.edu>
2018-01-31 21:08:22 -0800

    add intent

--- app/src/main/java/com/game/team9/slidingpuzzle/MathPlayerName.java
+++ app/src/main/java/com/game/team9/slidingpuzzle/MathPlayerName.java
@@ -1,7 +1,10 @@
 package com.game.team9.slidingpuzzle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MathPlayerName extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MathPlayerName extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math_player_name);
+        Button button_mathstart = (Button)findViewById(R.id.mathstart);
+        button_mathstart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_math_player_name = new Intent(MathPlayerName.this, MathModeStart.class);
+                startActivity(intent_math_player_name);
+            }
+        });
     }
+
+
 }
