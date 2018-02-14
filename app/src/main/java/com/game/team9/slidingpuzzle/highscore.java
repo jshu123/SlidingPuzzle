557446143c318e3c0a3b6fdcedc597cb9ee4f5ac
jshu123 <jshu@pdx.edu>
2018-02-14 10:11:51 -0800

    highscore class

---
+++ app/src/main/java/com/game/team9/slidingpuzzle/highscore.java
@@ -0,0 +1,49 @@
+package com.game.team9.slidingpuzzle;
+/*
+ * Copyright (c) 2018. $user
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Created by gongb on 2/12/2018.
+ */
+
+public class highscore {
+    private int index;
+    private String name;
+    private int score;
+
+    public highscore(int index, String name, int score) {
+        this.index = index;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
