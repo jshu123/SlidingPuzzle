96ae8c88094d3bcc5c7e2a01b307d2278456b6bd
jshu123 <jshu@pdx.edu>
2018-02-23 12:06:21 -0800

    add pause/resume to timer

--- app/src/main/java/com/game/team9/slidingpuzzle/MathSinglePlayerActivity.java
+++ app/src/main/java/com/game/team9/slidingpuzzle/MathSinglePlayerActivity.java
@@ -3,11 +3,13 @@ package com.game.team9.slidingpuzzle;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,9 +25,10 @@ public class MathSinglePlayerActivity extends BaseMathActivity {
 
     private TextView m_ScoreView;
     private int m_Score;
-
+    private Button m_Pause;
     private MathModeView m_Game;
     private Chronometer m_Timer;
+    private long lastPause;
     private byte[] m_Tiles;
 
     private final Set<Equation> m_Hist = new HashSet<>();
@@ -37,8 +40,10 @@ public class MathSinglePlayerActivity extends BaseMathActivity {
         setContentView(R.layout.activity_single_player);
         m_ScoreView = findViewById(R.id.hostScoreText);
         m_Game = findViewById(R.id.playerView);
+        m_Pause = findViewById(R.id.pauseButton);
         m_Score = 0;
         m_ScoreView.setText("0");
+        m_Pause.setText("Pause");
 
 
 
@@ -47,16 +52,36 @@ public class MathSinglePlayerActivity extends BaseMathActivity {
         m_Game.Initialize(getBoard(), this);
         m_Timer = findViewById(R.id.chronometer);
         m_Timer.start();
+
     }
 
 
     public void onGiveupClicked(View view)
     {
 
-        //badToast(R.string.invalid_eq);
         onGameEnded();
-    }
 
+    }
+    public void onPauseClicked(View view)
+    {
+
+        String pstring = m_Pause.getText().toString();
+        if(pstring.equals("Pause")){
+            m_Pause.setText("Resume");
+            lastPause = SystemClock.elapsedRealtime();
+            m_Timer.stop();
+
+        }
+        else{
+            m_Pause.setText("Pause");
+            m_Timer.setBase(m_Timer.getBase()+ SystemClock.elapsedRealtime()-lastPause);
+            m_Timer.start();
+
+
+        }
+        //badToast(R.string.invalid_eq);
+
+    }
     @Override
     protected void onGameEnded(String msg) {
         m_User.setScore(m_Score);
