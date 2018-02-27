3fc1678f8646e0e36de0e5d631dbbcd6b74f5bd6
Prakhar Sinha <prakhar1993@gmail.com>
2018-02-27 01:19:28 -0800

    Splash Screen

---
+++ app/src/main/java/com/game/team9/slidingpuzzle/SplashActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2018. $user
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.game.team9.slidingpuzzle;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private GifImageView gifImageView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        gifImageView=(GifImageView)findViewById(R.id.gifImageView);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+
+        progressBar.setVisibility((progressBar.VISIBLE));
+
+        try{
+            InputStream inputStream= getAssets().open("splashscreen.gif");
+            byte[] bytes= IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+
+        catch(IOException ex)
+        {
+
+
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                SplashActivity.this.finish();
+            }
+        },3000);
+    }
+}
